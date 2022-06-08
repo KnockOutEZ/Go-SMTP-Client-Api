87d5f25e7740bb5af19c4a2474f1a9dae2346459
KnockOutEZ <70368615+KnockOutEZ@users.noreply.github.com>
2022-06-08 19:32:29 +0600

    initial launch

--- main.go
+++ main.go
@@ -4,23 +4,29 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"strings"
 
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
 	han "github.com/tusharhow/handlers"
 )
 
 func main() {
+	// Load .env file to use the environment variable
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
 
 	r := mux.NewRouter()
-	p := ":8080"
+	p := os.Getenv("PORT")
 
 	r.HandleFunc("/sendmail", han.SendMail).Methods("POST")
 
-	fmt.Println("Server is running on port: ", strings.Split(p, ":")[1])
+	fmt.Println("Server is running on port: ", strings.Split(":"+p, ":")[1])
 
-	// han.EmailDatas = append(han.EmailDatas, han.EmailData{ID: "33", EmailTo: "kaka400068@gmail.com", EmailFrom: "kaka400069@gmail.com", EmailSub:"test",EmailBody:"testify",EmailPassword:"greenthree135790"})
-
-	log.Fatal(http.ListenAndServe(p, r))
+	log.Fatal(http.ListenAndServe(":"+p, r))
 
 }
